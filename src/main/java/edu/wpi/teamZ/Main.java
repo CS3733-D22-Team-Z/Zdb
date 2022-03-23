67e5a92f374bc719141d9514f4c064e54d0af0d0
IAmMe02 <caseywohlers@gmail.com>
2022-03-22 20:24:18 -0400

    Initial UI selections + exit

--- src/main/java/edu/wpi/teamZ/Main.java
+++ src/main/java/edu/wpi/teamZ/Main.java
@@ -1,9 +1,60 @@
 package edu.wpi.teamZ;
 
+import static java.lang.System.exit;
+
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
+    while (true) {
+      printUI();
+      takeAction();
+    }
+
     // App.launch(App.class, args);
+  }
+
+  public static void printUI() {
+    System.out.println("1 – Location Information");
+    System.out.println("2 – Change Floor and Type");
+    System.out.println("3 – Enter Location");
+    System.out.println("4 – Delete Location");
+    System.out.println("5 – Save Locations to CSV file");
+    System.out.println("6 – Exit Program");
+  }
+
+  public static void takeAction() {
+    Scanner in = new Scanner(System.in);
+    int selection = 0;
+    while (selection <= 0 || selection >= 7) {
+      System.out.println("Selection? ");
+      selection = in.nextInt();
+      if (selection <= 0 || selection >= 7) {
+        System.out.println("Invalid. Try again.");
+      }
+    }
+
+    switch (selection) {
+      case 1:
+        // TODO: print info
+        break;
+      case 2:
+        // TODO: edit info
+        break;
+      case 3:
+        // TODO: new info
+        break;
+      case 4:
+        // TODO: delete info
+        break;
+      case 5:
+        // TODO: export
+        break;
+      case 6:
+        exit(0);
+        break;
+    }
   }
 }
